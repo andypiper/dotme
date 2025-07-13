ed25889f3a80ef864511f748d220a08c7e7b19f8
Andy Piper <andypiper@users.noreply.github.com>
2025-07-13 20:49:18 +0100

    OpenMoji, some minor updates.
    Signed-off-by: Andy Piper <andypiper@users.noreply.github.com>

--- templates/footer.js
+++ templates/footer.js
@@ -1,7 +1,7 @@
 export const renderFooter = () => `<footer class="footer" role="contentinfo">
   <div class="tagline">
-    <strong>formerly, built on Glitch in Bio</strong><br />
-    I remade it, my way.
+    <strong>dotme</strong> - an evolution of Glitch in Bio<br />
+    I remade it, my way. Like it? Code <a href="https://github.com/andypiper/dotme">available on GitHub</a>.
   </div>
 </footer>
 `;
